7b8e2e2fb61a6a5e57f28cf11bf835055b1d109c
Ignacy Moryc <imoryc@gmail.com>
2019-11-20 18:08:43 +0100

    Check board field color

--- WarcabyApp.Tests/WarcabyApp_Board.cs
+++ WarcabyApp.Tests/WarcabyApp_Board.cs
@@ -3,37 +3,49 @@ using WarcabyApp;
 
 namespace WarcabyApp.UnitTests.Services
 {
-    public class WarcabyApp_Board
+  public class WarcabyApp_Board
+  {
+    private Board _board;
+
+    [Fact]
+    public void DefaultBoardSize_Is8by8()
     {
-        private Board _board;
-
-        [Fact]
-        public void DefaultBoardSize_Is8by8()
-        {
-            _board = new Board();
-            var size = _board.Size;
-            Assert.Equal(8, size);
-        }
-
-        [Theory]
-        [InlineData(1)]
-        [InlineData(5)]
-        [InlineData(20)]
-        public void BoardSizeIsAccepted(int size)
-        {
-            _board = new Board(size);
-            var actualSize = _board.Size;
-            Assert.Equal(size, actualSize);
-        }
-
-        [Theory]
-        [InlineData(-1)]
-        [InlineData(0)]
-        public void RaiseExceptionWhenWrongBoardSizeIsUsed(int size)
-        {
-          Assert.Throws<System.ArgumentException>(() => new Board(size));
-        }
-
-
+      _board = new Board();
+      var size = _board.Size;
+      Assert.Equal(8, size);
     }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(5)]
+    [InlineData(20)]
+    public void BoardSizeIsAccepted(int size)
+    {
+      _board = new Board(size);
+      var actualSize = _board.Size;
+      Assert.Equal(size, actualSize);
+    }
+
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(0)]
+    public void RaiseExceptionWhenWrongBoardSizeIsUsed(int size)
+    {
+      Assert.Throws<System.ArgumentException>(() => new Board(size));
+    }
+
+
+    [Theory]
+    [InlineData(0, 0, FieldColor.Black)]
+    [InlineData(1, 1, FieldColor.Black)]
+    [InlineData(3, 1, FieldColor.Black)]
+    [InlineData(3, 0, FieldColor.White)]
+    [InlineData(0, 1, FieldColor.White)]
+    [InlineData(7, 7, FieldColor.Black)]
+    public void RecognizesBoardsFieldColorCorrectly(int x, int y, FieldColor color) {
+      _board = new Board(8);
+
+      Assert.Equal(_board.GetFieldColorAt(x, y), color);
+    }
+  }
 }
--- WarcabyApp/Board.cs
+++ WarcabyApp/Board.cs
@@ -1,22 +1,42 @@
 namespace WarcabyApp
 {
-    public class Board
+  public enum FieldColor { White, Black };
+
+  public class Board
+  {
+
+    private readonly int DEFAULT_SIZE = 8;
+    public int Size { get; }
+
+    public Board()
     {
-        private readonly int DEFAULT_SIZE = 8;
-        public int Size { get; }
-
-        public Board()
-        {
-            this.Size = DEFAULT_SIZE;
-        }   
-
-        public Board(int n)
-        {
-            if (n <= 0)
-            {
-                throw new System.ArgumentException("Valid board size is > 0");
-            }
-            this.Size = n;
-        }
+      this.Size = DEFAULT_SIZE;
     }
+
+    public Board(int n)
+    {
+      if (n <= 0)
+      {
+        throw new System.ArgumentException("Valid board size is > 0");
+      }
+      this.Size = n;
+    }
+
+    public FieldColor GetFieldColorAt(int x, int y)
+    {
+      if (x > this.Size - 1) {
+        throw new System.ArgumentException($"{x} is out of board bounds {this.Size}x{this.Size}");
+      }
+      if (y > this.Size - 1) {
+        throw new System.ArgumentException($"{y} is out of board bounds {this.Size}x{this.Size}");
+      }
+
+      if ((x % 2) != (y % 2)) {
+        return FieldColor.White;
+      } else {
+        return FieldColor.Black;
+      }
+
+    }
+  }
 }
