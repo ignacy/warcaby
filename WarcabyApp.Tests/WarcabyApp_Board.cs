88db6edbf39635750ca20903d68f05db86964de4
Ignacy Moryc <imoryc@gmail.com>
2019-11-20 19:15:00 +0100

    Knows how to set a pown on the board

--- WarcabyApp.Tests/WarcabyApp_Board.cs
+++ WarcabyApp.Tests/WarcabyApp_Board.cs
@@ -3,67 +3,77 @@ using WarcabyApp;
 
 namespace WarcabyApp.UnitTests.Services
 {
-  public class WarcabyApp_Board
-  {
-    private Board _board;
-
-    [Fact]
-    public void DefaultBoardSize_Is8by8()
+    public class WarcabyApp_Board
     {
-      _board = new Board();
-      var size = _board.Size;
-      Assert.Equal(8, size);
-    }
+        private Board _board;
 
-    [Theory]
-    [InlineData(1)]
-    [InlineData(5)]
-    [InlineData(20)]
-    public void BoardSizeIsAccepted(int size)
-    {
-      _board = new Board(size);
-      var actualSize = _board.Size;
-      Assert.Equal(size, actualSize);
-    }
+        [Fact]
+        public void DefaultBoardSize_Is8by8()
+        {
+            _board = new Board();
+            var size = _board.Size;
+            Assert.Equal(8, size);
+        }
 
-    [Theory]
-    [InlineData(-1)]
-    [InlineData(0)]
-    public void RaiseExceptionWhenWrongBoardSizeIsUsed(int size)
-    {
-      Assert.Throws<System.ArgumentException>(() => new Board(size));
-    }
+        [Theory]
+        [InlineData(1)]
+        [InlineData(5)]
+        [InlineData(20)]
+        public void BoardSizeIsAccepted(int size)
+        {
+            _board = new Board(size);
+            var actualSize = _board.Size;
+            Assert.Equal(size, actualSize);
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        public void RaiseExceptionWhenWrongBoardSizeIsUsed(int size)
+        {
+            Assert.Throws<System.ArgumentException>(() => new Board(size));
+        }
 
 
-    [Theory]
-    [InlineData(0, 0, FieldColor.Black)]
-    [InlineData(1, 1, FieldColor.Black)]
-    [InlineData(3, 1, FieldColor.Black)]
-    [InlineData(3, 0, FieldColor.White)]
-    [InlineData(0, 1, FieldColor.White)]
-    [InlineData(7, 7, FieldColor.Black)]
-    public void RecognizesBoardsFieldColorCorrectly(int x, int y, FieldColor color) {
-      _board = new Board(8);
+        [Theory]
+        [InlineData(0, 0, FieldColor.Black)]
+        [InlineData(1, 1, FieldColor.Black)]
+        [InlineData(3, 1, FieldColor.Black)]
+        [InlineData(3, 0, FieldColor.White)]
+        [InlineData(0, 1, FieldColor.White)]
+        [InlineData(7, 7, FieldColor.Black)]
+        public void RecognizesBoardsFieldColorCorrectly(int x, int y, FieldColor color)
+        {
+            _board = new Board(8);
 
-      Assert.Equal(_board.GetFieldColorAt(x, y), color);
-    }
+            Assert.Equal(_board.GetFieldColorAt(x, y), color);
+        }
 
-    [Fact]
-    public void ReprezentsBoardAsArrayOfString() {
-      _board = new Board(4);
+        [Fact]
+        public void ReprezentsBoardAsArrayOfString()
+        {
+            _board = new Board(4);
 
-      Assert.Equal(
-                   _board.Position,
-                   new string[,] {
+            Assert.Equal(
+                         _board.Position,
+                         new string[,] {
                      { "b", "w", "b", "w" },
                      { "w", "b", "w", "b" },
                      { "b", "w", "b", "w" },
                      { "w", "b", "w", "b" }
-                   }
-                   );
+                         }
+                         );
+        }
 
 
-
+      [Theory]
+      [InlineData(0, 0, PawnColor.White, "P")]
+      [InlineData(0, 0, PawnColor.Black, "p")]
+      [InlineData(1, 1, PawnColor.White, "P")]
+       public void CanSetAPawnOnTheBoard(int x, int y, PawnColor color, string expected) {
+            _board = new Board(4);
+            _board.SetPownAt(x, y, color);
+            Assert.Equal(expected, _board.Position[x, y]);
+      }
     }
-  }
 }
--- WarcabyApp/Board.cs
+++ WarcabyApp/Board.cs
@@ -2,71 +2,103 @@ using System;
 
 namespace WarcabyApp
 {
-  public enum FieldColor { White, Black };
-  public enum PawnColor { White, Black };
+    public enum FieldColor { White, Black };
+    public enum PawnColor { White, Black };
 
-  public class Board
-  {
-
-    private readonly int DEFAULT_SIZE = 8;
-    public int Size { get; }
-    public string[,] Position { get; }
-
-    public Board()
+    public class Board
     {
-      this.Size = DEFAULT_SIZE;
-      this.Position = new string[this.Size, this.Size];
-      this.colorFields();
-    }
 
-    public Board(int n)
-    {
-      if (n <= 0)
-      {
-        throw new System.ArgumentException("Valid board size is > 0");
-      }
-      this.Size = n;
-      this.Position = new string[this.Size, this.Size];
-      this.colorFields();
-    }
+        private readonly int DEFAULT_SIZE = 8;
+        public int Size { get; }
+        public string[,] Position { get; }
 
-    public void SetPownAt(int x, int y, PawnColor color) {
-
-    }
-
-    public FieldColor GetFieldColorAt(int x, int y)
-    {
-      if (x > this.Size - 1) {
-        throw new System.ArgumentException($"{x} is out of board bounds {this.Size}x{this.Size}");
-      }
-      if (y > this.Size - 1) {
-        throw new System.ArgumentException($"{y} is out of board bounds {this.Size}x{this.Size}");
-      }
-
-      if ((x % 2) != (y % 2)) {
-        return FieldColor.White;
-      } else {
-        return FieldColor.Black;
-      }
-    }
-
-    public void colorFields() {
-      for (int i = 0; i < this.Size; i++) {
-        for (int j = 0; j < this.Size; j++) {
-          this.Position[i, j] = (this.GetFieldColorAt(i, j) == FieldColor.White) ? "w" : "b";
-        }
-      }
-    }
-
-    public void PrintToOut() {
-      for (int i = this.Size - 1; i >= 0; i--) {
-        for (int j = 0; j < this.Size; j++) {
-          Console.Write(this.Position[i,j]);
-          Console.Write("\t");
+        public Board()
+        {
+            this.Size = DEFAULT_SIZE;
+            this.Position = new string[this.Size, this.Size];
+            this.colorFields();
         }
 
-        Console.Write("\n");
-      }
+        public Board(int n)
+        {
+            if (n <= 0)
+            {
+                throw new System.ArgumentException("Valid board size is > 0");
+            }
+            this.Size = n;
+            this.Position = new string[this.Size, this.Size];
+            this.colorFields();
+        }
+
+        public void SetPownAt(int x, int y, PawnColor color)
+        {
+            if (x > this.Size - 1)
+            {
+                throw new System.ArgumentException($"{x} is out of board bounds {this.Size}x{this.Size}");
+            }
+            if (y > this.Size - 1)
+            {
+                throw new System.ArgumentException($"{y} is out of board bounds {this.Size}x{this.Size}");
+            }
+            if (this.Position[x, y] == "P" || this.Position[x, y] == "p")
+            {
+                throw new System.ArgumentException($"({x},{y}) is already occupied by {this.Position[x, y]}");
+            }
+
+            if (color == PawnColor.White)
+            {
+                this.Position[x, y] = "P";
+            }
+            else
+            {
+                this.Position[x, y] = "p";
+            }
+        }
+
+        public FieldColor GetFieldColorAt(int x, int y)
+        {
+            if (x > this.Size - 1)
+            {
+                throw new System.ArgumentException($"{x} is out of board bounds {this.Size}x{this.Size}");
+            }
+            if (y > this.Size - 1)
+            {
+                throw new System.ArgumentException($"{y} is out of board bounds {this.Size}x{this.Size}");
+            }
+
+            if ((x % 2) != (y % 2))
+            {
+                return FieldColor.White;
+            }
+            else
+            {
+                return FieldColor.Black;
+            }
+        }
+
+        public void colorFields()
+        {
+            for (int i = 0; i < this.Size; i++)
+            {
+                for (int j = 0; j < this.Size; j++)
+                {
+                    this.Position[i, j] = (this.GetFieldColorAt(i, j) == FieldColor.White) ? "w" : "b";
+                }
+            }
+        }
+
+        public void PrintToOut()
+        {
+            for (int i = this.Size - 1; i >= 0; i--)
+            {
+                for (int j = 0; j < this.Size; j++)
+                {
+                    Console.Write(this.Position[i, j]);
+                    Console.Write("\t");
+                }
+
+                Console.Write("\n");
+            }
+        }
     }
-  }
 }
